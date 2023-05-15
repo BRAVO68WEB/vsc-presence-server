e0a4900a26279dd3e3e9b989cf4ae7fadaf7062c
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-05-15 12:24:40 +0530

    Added Server Fuctions  + v1.0.1

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import express from 'express';
 import WebSocket from 'ws';
 import Redis from 'ioredis';
@@ -20,21 +21,74 @@ wss.on('connection', (ws: WebSocket) => {
   ws.on('message', async (message: string) => {
     try {
       // Store data in Redis as a single key
-      await redis.set('data', message);
+      const { pubkey, presence } = JSON.parse(message);
+      console.log(pubkey, presence);
+      await redis.set('presence_' + pubkey, JSON.stringify(presence));
     } catch (error) {
       console.error('Failed to store data in Redis:', error);
     }
   });
 });
 
-// REST API to retrieve the stored value
-app.get('/data', async (req, res) => {
+app.get('/register', async (req, res) => {
+  try {
+    // Store data in Redis as a single key
+    const { email } = req.query;
+    if (!email) {
+      res.send('No email provided in query');
+    } else {
+      const pubkey =
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15);
+      const fetchkey = Buffer.from(email.toString()).toString('base64');
+      await redis.hset(`publisher`, fetchkey, pubkey);
+      await redis.hset(`subscriber`, pubkey, fetchkey);
+      res.send({
+        fetchkey: fetchkey,
+        publisherkey: pubkey,
+      });
+    }
+    res.json({ data: 'test' });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to store data' });
+  }
+});
+
+// REST API to get fetchKey using publisher key
+app.get('/fetchkey/:publisherkey', async (req, res) => {
   try {
     // Retrieve the value of the 'data' key from Redis
-    const value = await redis.get('data');
-    res.json({ data: value });
+    // Check if the publisher key exists
+    const value = await redis.hget('subscriber', req.params.publisherkey);
+    if (!value) {
+      res.status(404).json({ data: 'Publisher key not found' });
+      return;
+    } else {
+      const encEmail = value.toString();
+      res.json({ data: encEmail });
+      return;
+    }
   } catch (error) {
-    console.error('Failed to retrieve data from Redis:', error);
+    res.status(500).json({ error: 'Failed to retrieve data' });
+  }
+});
+
+// REST API to retrieve the stored value
+app.get('/presence/:fetchkey', async (req, res) => {
+  try {
+    const newLocal = 'publisher';
+    // Retrieve the value of the 'data' key from Redis
+    const pubkey = await redis.hget(newLocal, req.params.fetchkey);
+    const value: any = await redis.get(`presence_${pubkey}`);
+    let output = JSON.parse(value);
+    output = {
+      ...output,
+      currentTimestamp: Date.now(),
+      timeDiff: Date.now() - output.startTimestamp,
+      iconURL: `https://raw.githubusercontent.com/PKief/vscode-material-icon-theme/main/icons/${output.smallImageKey}.svg`,
+    };
+    res.json({ data: output });
+  } catch (error) {
     res.status(500).json({ error: 'Failed to retrieve data' });
   }
 });
